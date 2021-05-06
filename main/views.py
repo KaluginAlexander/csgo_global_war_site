58cfa33b813602bb03bce2dda73c939dac7e4af6
KaluginAlexander <kalugin.alex18@gmail.com>
2021-05-07 00:44:32 +0500

    проверка заказа на уникальность

--- main/views.py
+++ main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from main.models import MenuContent
+# from main.models import MenuContent
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import hmac
-import hashlib
-import codecs
-import json
+import hashlib, codecs, hmac
+import json, sys, os
+
+
+slash = ('/', '\\')['\\' in os.path.dirname(__file__)]
+botPath = slash.join(os.path.dirname(__file__).split(slash)[0:-2]) + slash + 'CSGO_vk_bot'
+invoicesPath = botPath + '/core/bot/database/data'
+sys.path.insert(0, botPath)
+
+from core.bot.database import main as botDB
 
 # Константы
 SECRET_KEY = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjFnZnpjNC0wMCIsInVzZXJfaWQiOiI3OTAwMDIzOTUyMyIsInNlY3JldCI6ImFmYjFjNGRiYjllMGMwZGQ3OGFiYWIwOGJlZTBlZmUxZWMyZmU0OGYyZTc0YTk1NTY3MmFiNzgwMDAzNDY2NmUifX0='
+makedBills = []
+
 
 # Create your views here.
 def index(request):
@@ -27,7 +35,6 @@ def index(request):
 
 @csrf_exempt
 def notify(request):
-    print('=================')
     if request.method == 'POST':
         
         bill = json.loads(request.body)['bill']
@@ -42,9 +49,15 @@ def notify(request):
         accept = hmac.new(codecs.encode(SECRET_KEY), msg=codecs.encode(invoice_parameters), digestmod=hashlib.sha256).hexdigest()
 
         # проверка подлиности
-        if request.headers['X-Api-Signature-SHA256'] == accept:
+        if request.headers['X-Api-Signature-SHA256'] == accept and billId not in makedBills:
 
             # Получаем аккаунт
             nickname = bill['customer']['account']
+
+            # Добавляем в бд запись
+            main.request('delay', f"INSERT INTO Invoices VALUES('{nickname}', {amount})", invoicesPath)
+
+            # Помечаем заказ выполненым
+            makedBills.append(billId)
 
     return HttpResponse(status=200)
