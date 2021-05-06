dbf81af9636063dc699b16395636e18eb7f253e8
KaluginAlexander <kalugin.alex18@gmail.com>
2021-05-06 22:14:55 +0500

    Получение уведомлений от qiwi

--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from main.models import MenuContent
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import hmac
+import hashlib
+import codecs
+import json
+
+# Константы
+SECRET_KEY = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjFnZnpjNC0wMCIsInVzZXJfaWQiOiI3OTAwMDIzOTUyMyIsInNlY3JldCI6ImFmYjFjNGRiYjllMGMwZGQ3OGFiYWIwOGJlZTBlZmUxZWMyZmU0OGYyZTc0YTk1NTY3MmFiNzgwMDAzNDY2NmUifX0='
 
 # Create your views here.
 def index(request):
-    
+
     context = {
         'menu': {
             'info': MenuContent.objects.get(id=1),
@@ -14,3 +23,28 @@ def index(request):
     }
 
     return render(request, 'main/index.html', context)
+
+
+@csrf_exempt
+def notify(request):
+    print('=================')
+    if request.method == 'POST':
+        
+        bill = json.loads(request.body)['bill']
+        
+        # Получаем параметры
+        amount = bill['amount']['value']
+        billId = bill['billId']
+        siteId = bill['siteId']
+        status = bill['status']['value']
+        
+        invoice_parameters = f"RUB|{amount}|{billId}|{siteId}|{status}"
+        accept = hmac.new(codecs.encode(SECRET_KEY), msg=codecs.encode(invoice_parameters), digestmod=hashlib.sha256).hexdigest()
+
+        # проверка подлиности
+        if request.headers['X-Api-Signature-SHA256'] == accept:
+
+            # Получаем аккаунт
+            nickname = bill['customer']['account']
+
+    return HttpResponse(status=200)
