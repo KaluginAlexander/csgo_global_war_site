c45d9f5f1d2aa4ac80344c53d2649159803e19e8
KaluginAlexander <kalugin.alex18@gmail.com>
2021-05-15 10:17:31 +0500

    Добавил бек платежа

--- main/views.py
+++ main/views.py
@@ -1,21 +1,36 @@
 from typing_extensions import ParamSpecKwargs
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from main.models import AboutContent, MenuContent
 from shop.models import Product
 
 # Create your views here.
 def index(request):
 
-    context = {
-        'menu': {
-            'about': MenuContent.objects.get(id=2),
-            'shop': MenuContent.objects.get(id=3),
-            'faq': MenuContent.objects.get(id=4),
-        },
-        'page': {
-            'about': AboutContent.objects.get(id=1)
-        },
-        'products': Product.objects.all()
-    }
+    if request.method == "POST":
+        
+        data = request.POST
 
-    return render(request, 'main/index.html', context)
+        if 'nickname' in data.keys() and 'productId' in data.keys():
+            pay_url = Product.get_pay_url(data['nickname'], data['productId'])
+
+            return redirect(pay_url)
+
+        else:
+            return redirect(reverse('home'))
+
+    else:
+
+        context = {
+            'menu': {
+                'about': MenuContent.objects.get(id=2),
+                'shop': MenuContent.objects.get(id=3),
+                'faq': MenuContent.objects.get(id=4),
+            },
+            'page': {
+                'about': AboutContent.objects.get(id=1)
+            },
+            'products': Product.objects.all()
+        }
+
+        return render(request, 'main/index.html', context)
